ba06813c27ca06ebd139e627b9c4cee8fbb0d55c
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-06 17:18:00 +0200

    add DEXTER module

--- fixtures/default.js
+++ fixtures/default.js
@@ -63,7 +63,7 @@ module.exports = _.merge({
              */
             input: {
                 //Replace VAR1 with the name of an expected input, and add more inputs as needed.
-                VAR1: 'foo'
+                publicationId: 'b45573563f5a'
             }
         }
     }
--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+var util = require('./util.js');
+var request = require('request').defaults({
+    baseUrl: 'https://api.medium.com/'
+});
+
+var pickInputs = {
+       'publicationId': { key: 'publicationId', validate: {req: true} }
+    }, pickOutputs = {
+        'publicationId': { keyName: 'data', fields: ['publicationId'] },
+        'userId': { keyName: 'data', fields: ['userId'] },
+        'role': { keyName: 'data', fields: ['role'] }
+    };
+
 module.exports = {
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +20,25 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var inputs = util.pickInputs(step, pickInputs),
+            validationErrors = util.checkValidateErrors(inputs, pickInputs),
+            token = dexter.environment('medium_access_token');
+
+        if (!token)
+            return this.fail('A [medium_access_token] environment variable is required for this module');
+
+        if (validationErrors)
+            return this.fail(validationErrors);
+
+        request.get({
+            uri: '/v1/publications/' + inputs.publicationId + '/contributors',
+            auth: { bearer: token },
+            json: true
+        }, function (error, response, body) {
+            if (error)
+                this.fail(error);
+            else
+                this.complete(util.pickOutputs(body, pickOutputs));
+        }.bind(this));
     }
 };
